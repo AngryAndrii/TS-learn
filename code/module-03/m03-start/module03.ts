2062837bb701ba8d07edae6572aab2f13601fcb6
Andrii Chychkan <mywork1728@gmail.com>
2024-09-05 16:40:57 +0300

    added module 03

--- code/module-03/m03-start/module03.ts
+++ code/module-03/m03-start/module03.ts
@@ -1,36 +1,56 @@
 /* Module 3: Implement interfaces in TypeScript
    Lab Start  */
 
+//    Объявите интерфейс Loan, который определяет два свойства: principal и interestRate.
+// Объявите интерфейс ConventionalLoan, который расширяет Loan и определяет дополнительное свойство, необходимое для обычного кредита, months.
+// Обновите обе функции, чтобы реализовать новые интерфейсы, и строго типизируйте параметры.
+
 /*  EXERCISE 1
     TODO: Declare the Loan interface. */
-
+interface Loan {
+    principle: number,
+    interestRate: number,
+}
 
 /*  TODO: Declare the ConventionalLoan interface. */
+interface ConventionalLoan extends Loan {
+    months: number
+}
 
+const myLoan: Loan = {
+    principle: 30000,
+    interestRate: 5,
+}
+
+const myData: ConventionalLoan = {
+    principle: 30000,
+    interestRate: 5,
+    months: 180
+}
 
 
 /*  TODO: Update the calculateInterestOnlyLoanPayment function. */
 
-function calculateInterestOnlyLoanPayment(principle, interestRate) {
+function calculateInterestOnlyLoanPayment(loanTerms: Loan): string {
     // Calculates the monthly payment of an interest only loan
-    let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
+    let interest = loanTerms.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
     let payment;
-    payment = principle * interest;
+    payment = loanTerms.principle * interest;
     return 'The interest only loan payment is ' + payment.toFixed(2);
 }
 
 /*  TODO: Update the calculateConventionalLoanPayment function. */
 
-function calculateConventionalLoanPayment(principle, interestRate, months) {
+function calculateConventionalLoanPayment(data: ConventionalLoan) {
     // Calculates the monthly payment of a conventional loan
-    let interest = interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
+    let interest = data.interestRate / 1200; // Calculates the Monthly Interest Rate of the loan
     let payment;
-    payment = principle * interest / (1 - (Math.pow(1 / (1 + interest), months)));
+    payment = data.principle * interest / (1 - (Math.pow(1 / (1 + interest), data.months)));
     return 'The conventional loan payment is ' + payment.toFixed(2);
 }
 
-let interestOnlyPayment = calculateInterestOnlyLoanPayment(30000, 5);
-let conventionalPayment = calculateConventionalLoanPayment(30000, 5, 180);
+let interestOnlyPayment = calculateInterestOnlyLoanPayment(myLoan);
+let conventionalPayment = calculateConventionalLoanPayment(myData);
 
 console.log(interestOnlyPayment);     //* Returns "The interest only loan payment is 125.00" 
 console.log(conventionalPayment);     //* Returns "The conventional loan payment is 237.24" 
